6e36ecb25cb0e4214a0e06ac630cf44b214308f7
Chance Baars <chance@ool-ad020102.dyn.optonline.net>
2018-04-19 10:02:57 -0400

    api test and partials

--- testing.js
+++ testing.js
@@ -1,7 +1,26 @@
-global.fetch = require('node-fetch');
-const cc = require('cryptocompare');
+// global.fetch = require('node-fetch');
+// const cc = require('cryptocompare');
+//
+//
+// cc.coinList().then(data => {
+//   console.log(data)
+// })
+// not good for use changed to coinmarketcap api!!!!!
 
 
-cc.coinList().then(data => {
-  console.log(data)
-})
+const fetch = require('node-fetch');
+
+function q1() {
+  fetch('https://api.coinmarketcap.com/v1/ticker/?limit=10')
+    .then(data => {
+      return data.json();
+    })
+    .then (data => {
+      console.log(data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+q1()
