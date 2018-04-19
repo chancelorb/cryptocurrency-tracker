7d8f0dc6ecc1382f6f1616c76b1d6c9d8f22a63b
Chance Baars <chance@Chance.home>
2018-04-19 07:05:38 -0400

    running server

---
+++ app/index.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const logger = require('morgan');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const PORT = 3000;
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
